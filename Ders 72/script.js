2a86abd70b7e1e5f97db0e2eab5c424d326ef22e
Rza <rza.rzayev.134@gmail.com>
2025-09-19 16:09:06 +0400

    Ders 72 JS Operatorlar / deyisenler

---
+++ Ders 72/script.js
@@ -0,0 +1,11 @@
+function fillPhoto(){
+    document.getElementById("photoId").src="nature.jpeg";
+}
+
+function addDoc(){
+    document.write("<button value='idk' style='width:100px; height:40px'> salam dunya</button>");
+}
+
+function showMessage(){
+    document.getElementById("displayId").innerHTML=prompt("Say something!");
+}
